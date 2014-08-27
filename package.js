fd0064c50bd2d6ef5f7a0b0c5489869eb929b93c
schnie <greg@differential.io>
2014-08-27 09:08:41 -0400

    change name

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: "di:uploader"
+  name: "schnie:uploader"
   summary: "Upload files to the clouds"
   version: "1.0.0"
   git: "https://github.com/Differential/meteor-uploader"
