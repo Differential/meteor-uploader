17b2dda6118ef0ed27e292455eb535675a06b970
schnie <greg@differential.io>
2014-08-27 09:05:57 -0400

    ready for 0.9.0

--- package.js
+++ package.js
@@ -1,51 +1,54 @@
 Package.describe({
-  summary: 'Upload files to the clouds'
+  name: "di:uploader"
+  summary: "Upload files to the clouds"
+  version: "1.0.0"
+  git: "https://github.com/Differential/meteor-uploader"
 });
 
-var both = ['client', 'server'];
+var both = ["client", "server"];
 
 Npm.depends({
-  'knox': '0.8.5',
-  'stream-buffers': '0.2.5'
+  "knox": "0.8.5",
+  "stream-buffers": "0.2.5"
 });
 
-Package.on_use(function (api) {
-  //Need service-configuration to use Meteor.method
+Package.onUse(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
+
   api.use([
-    'coffeescript',
-    'underscore',
-    'handlebars',
-    'templating',
-    'iron-router',
-    'streams',
-    'deps'
-    ], ['client', 'server']);
+    "coffeescript",
+    "underscore",
+    "handlebars",
+    "templating",
+    "arunoda:streams",
+    "deps"
+    ], both);
 
-  api.use(['less'], ['client']);
+  api.use(["less"], ["client"]);
 
-  api.use(['handlebars-server'], 'server');
+  // api.use(["handlebars-server"], "server");
 
-  api.add_files([
-    'lib/streams.coffee'
+  api.addFiles([
+    "lib/streams.coffee"
   ], both);
 
-  api.add_files([
-    'client/views/uploader.html',
-    'client/views/uploader.coffee',
-    'client/views/uploader.less'
-    ], 'client');
+  api.addFiles([
+    "client/views/uploader.html",
+    "client/views/uploader.coffee",
+    "client/views/uploader.less"
+    ], "client");
 
-  api.add_files([
-    'client/common/cors-configuration.handlebars',
-    'client/common/bucket-policy-configuration.handlebars',
-    'server/Uploader.coffee',
-    'server/server.coffee',
-    'server/routes.coffee'
-    ], 'server');
+  api.addFiles([
+    // "client/common/cors-configuration.handlebars",
+    // "client/common/bucket-policy-configuration.handlebars",
+    "server/Uploader.coffee",
+    "server/server.coffee",
+    "server/routes.coffee"
+    ], "server");
 
 
   if(api.export) {
-    // api.export('Knox','server');
-    // api.export(['S3'],['client', 'server']);
+    // api.export("Knox","server");
+    // api.export(["S3"],["client", "server"]);
   }
 });
